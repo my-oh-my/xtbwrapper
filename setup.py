03dd767224298b768890a54d50a8f5dedaae8093
me <21290320+my-oh-my@users.noreply.github.com>
2022-08-23 20:25:21 +0200

    Remove pandas (#1)
    Co-authored-by: Rafał Krasowski <my-oh-my@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='xtbwrapper',
-    version='0.1.0',
+    version='0.1.1',
     description='XTB Api Python wrapper for http://developers.xstore.pro/api/wrappers/2.5.0',
     author='my-oh-my',
     packages=['xtb.wrapper'],
-    install_requires=['pandas', 'setuptools']
+    install_requires=['setuptools']
 )
